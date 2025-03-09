579f1ff7c0de0c97777a5a7e0c96d9062b2e8987
yoram <yoramnag@gmail.com>
2025-03-09 12:39:59 +0200

    add new project

---
+++ src/main/java/com/example/mdClone/MdCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdCloneApplication.class, args);
+	}
+
+}
